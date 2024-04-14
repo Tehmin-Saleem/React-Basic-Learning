bd78f1d6c7691d972b15c12a52b187f65707a7b3
Tehmina Saleem <104255800+Tehmina-Saleem@users.noreply.github.com>
2024-04-14 17:23:20 +0500

    "Feat:Update chnages'

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css'
-import { FaqeerKyBety } from './components/FaqeerKyBety';
-import Products from './components/Products';
+// import { FaqeerKyBety } from './components/FaqeerKyBety';
+// import Products from './components/Products';
+import Counter from './components/Counter';
 // import FaqeerKyBety from './components/FaqeerKyBety'; // If no {} indicates, default import.
 // import MeiHonFaqeerkaBeta from './components/FaqeerKyBety'; // I can only change default components name.
 
 
 
 function App() {
-       const [name , setName] = useState("Abc");
-return <div>
-         <Products name ="isko zarroat nahiii phr bhii mei pass kar rha hon"/>
-       </div>
+      <Counter/>
 }
-
 export default App
